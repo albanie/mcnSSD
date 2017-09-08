126bb92983f70898ef8a160fe8df1b4af733a0cf
albanie <albanie@robots.ox.ac.uk>
2017-09-08 07:55:25 +0100

    clean up

--- compile_mcnSSD.m
+++ compile_mcnSSD.m
@@ -4,74 +4,74 @@ function compile_mcnSSD(varargin)
 % Copyright (C) 2017 Samuel Albanie
 % Licensed under The MIT License [see LICENSE.md for details]
 
-tokens = {vl_rootnn, 'matlab', 'mex', '.build', 'last_compile_opts.mat'} ;
-last_args_path = fullfile(tokens{:}) ; opts = {} ;
-if exist(last_args_path, 'file'), opts = {load(last_args_path)} ; end
-opts = selectCompileOpts(opts) ;
-vl_compilenn(opts{:}, varargin{:}, 'preCompileFn', @preCompileFn) ;
+  tokens = {vl_rootnn, 'matlab', 'mex', '.build', 'last_compile_opts.mat'} ;
+  last_args_path = fullfile(tokens{:}) ; opts = {} ;
+  if exist(last_args_path, 'file'), opts = {load(last_args_path)} ; end
+  opts = selectCompileOpts(opts) ;
+  vl_compilenn(opts{:}, varargin{:}, 'preCompileFn', @preCompileFn) ;
 
 % ------------------------------------------------------------------------
 function [opts, mex_src, lib_src, flags] = preCompileFn(opts, mex_src, ...
                                                         lib_src, flags)
 % ------------------------------------------------------------------------
-mcn_root = vl_rootnn() ;
-root = fullfile(fileparts(mfilename('fullpath')), 'matlab') ;
+  mcn_root = vl_rootnn() ;
+  root = fullfile(fileparts(mfilename('fullpath')), 'matlab') ;
 
-% Build inside the module path
-flags.src_dir = fullfile(root, 'src') ;
-flags.mex_dir = fullfile(root, 'mex') ;
-flags.bld_dir = fullfile(flags.mex_dir, '.build') ;
-implDir = fullfile(flags.bld_dir,'bits/impl') ;
-if ~exist(implDir, 'dir'), mkdir(implDir) ; end
-mex_src = {} ; lib_src = {} ; 
-if opts.enableGpu, ext = 'cu' ;  else, ext = 'cpp' ; end
+  % Build inside the module path
+  flags.src_dir = fullfile(root, 'src') ;
+  flags.mex_dir = fullfile(root, 'mex') ;
+  flags.bld_dir = fullfile(flags.mex_dir, '.build') ;
+  implDir = fullfile(flags.bld_dir,'bits/impl') ;
+  if ~exist(implDir, 'dir'), mkdir(implDir) ; end
+  mex_src = {} ; lib_src = {} ; 
+  if opts.enableGpu, ext = 'cu' ;  else, ext = 'cpp' ; end
 
-% Add mcn dependencies
-lib_src{end+1} = fullfile(mcn_root, 'matlab/src/bits', ['data.' ext]) ;
-lib_src{end+1} = fullfile(mcn_root, 'matlab/src/bits', ['datamex.' ext]) ;
-lib_src{end+1} = fullfile(mcn_root,'matlab/src/bits/impl/copy_cpu.cpp') ;
+  % Add mcn dependencies
+  lib_src{end+1} = fullfile(mcn_root, 'matlab/src/bits', ['data.' ext]) ;
+  lib_src{end+1} = fullfile(mcn_root, 'matlab/src/bits', ['datamex.' ext]) ;
+  lib_src{end+1} = fullfile(mcn_root,'matlab/src/bits/impl/copy_cpu.cpp') ;
 
-if opts.enableGpu
-  lib_src{end+1} = fullfile(mcn_root,'matlab/src/bits/datacu.cu') ;
-  lib_src{end+1} = fullfile(mcn_root,'matlab/src/bits/impl/copy_gpu.cu') ;
-end
+  if opts.enableGpu
+    lib_src{end+1} = fullfile(mcn_root,'matlab/src/bits/datacu.cu') ;
+    lib_src{end+1} = fullfile(mcn_root,'matlab/src/bits/impl/copy_gpu.cu') ;
+  end
 
-% ----------------------
-% include required files
-% ----------------------
-% old style include - leave as comment in case users have different setup
-%if ~isfield(flags, 'cc'), flags.cc = {inc} ; else, flags.cc{end+1} = inc ; end 
+  % ----------------------
+  % include required files
+  % ----------------------
+  % old style include - leave as comment in case users have different setup
+  %if ~isfield(flags, 'cc'), flags.cc = {inc} ; else, flags.cc{end+1} = inc ; end 
 
-% new style include
-inc = sprintf('-I"%s"', fullfile(mcn_root,'matlab','src')) ;
-inc_local = sprintf('-I"%s"', fullfile(root, 'src')) ;
-flags.base{end+1} = inc ; flags.base{end+1} = inc_local ;
+  % new style include
+  inc = sprintf('-I"%s"', fullfile(mcn_root,'matlab','src')) ;
+  inc_local = sprintf('-I"%s"', fullfile(root, 'src')) ;
+  flags.base{end+1} = inc ; flags.base{end+1} = inc_local ;
 
-% Add module files
-lib_src{end+1} = fullfile(root,'src/bits',['nnmultiboxdetector.' ext]) ;
-mex_src{end+1} = fullfile(root,'src',['vl_nnmultiboxdetector.' ext]) ;
+  % Add module files
+  lib_src{end+1} = fullfile(root,'src/bits',['nnmultiboxdetector.' ext]) ;
+  mex_src{end+1} = fullfile(root,'src',['vl_nnmultiboxdetector.' ext]) ;
 
-% CPU-specific files
-lib_src{end+1} = fullfile(root,'src/bits/impl/multiboxdetector_cpu.cpp') ;
+  % CPU-specific files
+  lib_src{end+1} = fullfile(root,'src/bits/impl/multiboxdetector_cpu.cpp') ;
 
-% GPU-specific files
-if opts.enableGpu
-  lib_src{end+1} = fullfile(root,'src/bits/impl/multiboxdetector_gpu.cu') ;
-end
+  % GPU-specific files
+  if opts.enableGpu
+    lib_src{end+1} = fullfile(root,'src/bits/impl/multiboxdetector_gpu.cu') ;
+  end
 
 % -------------------------------------
 function opts = selectCompileOpts(opts) 
 % -------------------------------------
-keep = {'enableGpu', 'enableImreadJpeg', 'enableCudnn', 'enableDouble', ...
-        'imageLibrary', 'imageLibraryCompileFlags', ...
-        'imageLibraryLinkFlags', 'verbose', 'debug', 'cudaMethod', ...
-        'cudaRoot', 'cudaArch', 'defCudaArch', 'cudnnRoot', 'preCompileFn'} ; 
-s = opts{1} ;
-f = fieldnames(s) ;
-for i = 1:numel(f)
-  if ~ismember(f{i}, keep)
-    s = rmfield(s, f{i}) ;
+  keep = {'enableGpu', 'enableImreadJpeg', 'enableCudnn', 'enableDouble', ...
+          'imageLibrary', 'imageLibraryCompileFlags', ...
+          'imageLibraryLinkFlags', 'verbose', 'debug', 'cudaMethod', ...
+          'cudaRoot', 'cudaArch', 'defCudaArch', 'cudnnRoot', 'preCompileFn'} ; 
+  s = opts{1} ;
+  f = fieldnames(s) ;
+  for i = 1:numel(f)
+    if ~ismember(f{i}, keep)
+      s = rmfield(s, f{i}) ;
+    end
   end
-end
-opts = {s} ;
+  opts = {s} ;
 
--- ssd_demo.m
+++ ssd_demo.m
@@ -1,116 +1,92 @@
 function ssd_demo(varargin)
 %SSD_DEMO Minimalistic demonstration of the SSD detector
+%   SSD_DEMO an object detection demo with a Single Shot Detector
+%
+%   SSD_DEMO(..., 'option', value, ...) accepts the following
+%   options:
+%
+%   `modelPath`:: ''
+%    Path to a valid R-FCN matconvnet model. If none is provided, a model
+%    will be downloaded.
+%
+%   `gpus`:: []
+%    Device on which to run network 
+%
+% Copyright (C) 2017 Samuel Albanie
+% Licensed under The MIT License [see LICENSE.md for details]
 
-% Setup MatConvNet and SSD
-curr = fileparts(mfilename('fullpath')) ;
-run(fullfile(curr, '../../matlab/vl_setupnn.m')) ;
-setup_mcnSSD ;
+  opts.gpu = 1 ;
+  opts.modelPath = '' ;
+  opts = vl_argparse(opts, varargin) ;
 
-opts.modelPath = '' ;
-opts.gpu = [1] ;
-opts.batchSize = 1 ;
+  % The network is trained to prediction occurences
+  % of the following classes from the pascal VOC challenge
+  classes = {'background', 'aeroplane', 'bicycle', 'bird', ...
+     'boat', 'bottle', 'bus', 'car', 'cat', 'chair', 'cow', 'diningtable', ...
+     'dog', 'horse', 'motorbike', 'person', 'pottedplant', 'sheep', ...
+     'sofa', 'train', 'tvmonitor'} ;
 
-% The network is trained to prediction occurences
-% of the following classes from the pascal VOC challenge
-opts.classes = {'none_of_the_above', ...
-    'aeroplane', ...
-    'bicycle', ...
-    'bird', ...
-    'boat', ...
-    'bottle', ...
-    'bus', ...
-    'car', ...
-    'cat', ...
-    'chair', ...
-    'cow', ...
-    'diningtable', ...
-    'dog', ...
-    'horse', ...
-    'motorbike', ...
-    'person', ...
-    'pottedplant', ...
-    'sheep', ...
-    'sofa', ...
-    'train', ...
-    'tvmonitor'} ;
+  % Load or download an example SSD model:
+  modelName = 'ssd-mcn-pascal-vggvd-300.mat' ;
+  paths = {opts.modelPath, ...
+           modelName, ...
+           fullfile(vl_rootnn, 'data', 'models-import', modelName)} ;
+  ok = find(cellfun(@(x) exist(x, 'file'), paths), 1) ;
 
-opts = vl_argparse(opts, varargin) ;
+  if isempty(ok)
+    fprintf('Downloading the SSD model ... this may take a while\n') ;
+    opts.modelPath = fullfile(vl_rootnn, 'data/models', modelName) ;
+    mkdir(fileparts(opts.modelPath)) ;
+    url = sprintf('http://www.robots.ox.ac.uk/~albanie/models/ssd/%s', modelName) ;
+    urlwrite(url, opts.modelPath) ;
+  else
+    opts.modelPath = paths{ok} ;
+  end
 
-% Load or download an example SSD model:
-modelName = 'ssd-mcn-pascal-vggvd-300.mat' ;
-paths = {opts.modelPath, ...
-         modelName, ...
-         fullfile(vl_rootnn, 'data', 'models-import', modelName)} ;
-ok = min(find(cellfun(@(x) exist(x, 'file'), paths))) ;
+  % Load the network and put it in test mode.
+  net = load(opts.modelPath) ;
+  net = dagnn.DagNN.loadobj(net);
+  net.mode = 'test' ;
 
-if isempty(ok)
-  fprintf('Downloading the SSD model ... this may take a while\n') ;
-  opts.modelPath = fullfile(vl_rootnn, 'data/models', modelName) ;
-  mkdir(fileparts(opts.modelPath)) ;
-  url = sprintf('http://www.robots.ox.ac.uk/~albanie/models/ssd/%s', modelName) ;
-  urlwrite(url, opts.modelPath) ;
-else
-  opts.modelPath = paths{ok} ;
-end
+  % Load test image
+  im = single(imread('test.jpg')) ; im = imresize(im, [300 300]) ;
 
-% Load the network and put it in test mode.
-net = load(opts.modelPath) ;
-net = dagnn.DagNN.loadobj(net);
-net.mode = 'test' ;
+  % Evaluate network either on CPU or GPU.
+  if numel(opts.gpu) > 0
+    gpuDevice(opts.gpu) ; net.move('gpu') ; im = gpuArray(im) ;
+  end
 
-% Load test image
-im = single(imread('test.jpg')) ;
-im = imresize(im, [300 300]) ;
+  % tell the network to store the outputs
+  % of the prediction layer and do a forward pass
+  net.mode = 'test';
+  net.vars(end).precious = true ;
+  net.eval({'data', im}) ;
 
+  % gather the predictions from the network,
+  % and sort by confidence
+  preds = net.vars(end).value ;
 
-% Evaluate network either on CPU or GPU.
-if numel(opts.gpu) > 0
-  gpuDevice(opts.gpu) ;
-  net.move('gpu') ;
-  im = gpuArray(im) ;
-end
+  % check the last 
+  preds = preds(:,:,:,end) ;
+  [~, sortedIdx ] = sort(preds(:, 2), 'descend') ;
+  preds = preds(sortedIdx, :) ;
 
-% tell the network to store the outputs
-% of the prediction layer and do a forward pass
-net.mode = 'test';
-net.vars(end).precious = true ;
-net.eval({'data', im}) ;
+  % extract the most confident prediction
+  box = preds(1,3:end) ;
+  confidence = preds(1,2) ;
+  label = classes{preds(1,1)} ;
 
-% gather the predictions from the network,
-% and sort by confidence
-preds = net.vars(end).value ;
+  % return image to cpu for visualisation
+  if numel(opts.gpu) > 0, im = gather(im) ; end
 
-% check the last 
-preds = preds(:,:,:,end) ;
+  % diplay prediction as a sanity check
+  figure ; im = im / 255 ;
+  x = box(1) * size(im, 2) ; y = box(2) * size(im, 1) ;
+  width = box(3) * size(im, 2) - x ; height = box(4) * size(im, 1) - y ;
+  rectangle = [x y width height];
+  im = insertShape(im, 'Rectangle', rectangle, 'LineWidth', 3, 'Color', 'red') ;
+  imagesc(im) ;
+  title(sprintf('top SSD prediction: %s \n confidence: %f', label, confidence)) ;
 
-[~, sortedIdx ] = sort(preds(:, 2), 'descend') ;
-preds = preds(sortedIdx, :) ;
-
-% extract the most confident prediction
-box = preds(1,3:end) ;
-confidence = preds(1,2) ;
-label = opts.classes{preds(1,1)} ;
-
-% return image to cpu for visualisation
-if numel(opts.gpu) > 0
-  im = gather(im) ;
-end
-
-% diplay prediction as a sanity check
-figure ;
-im = im / 255 ;
-x = box(1) * size(im, 2) ;
-y = box(2) * size(im, 1) ;
-width = box(3) * size(im, 2) - x ;
-height = box(4) * size(im, 1) - y ;
-rectangle = [x y width height];
-im = insertShape(im, 'Rectangle', rectangle, ...
-                  'LineWidth', 3, ...
-                  'Color', 'red');
-imagesc(im) ;
-title(sprintf('top SSD prediction: %s \n confidence: %f', label, confidence)) ;
-
-% free up the GPU allocation
-if numel(opts.gpu) > 0
-  net.move('cpu') ;
-end
+  % free up the GPU allocation
+  if numel(opts.gpu) > 0, net.move('cpu') ; end
