3054cb0661f366aa8ef72967b852f9d050e42905
albanie <albanie@robots.ox.ac.uk>
2017-09-08 07:30:10 +0100

    updated compilation script

--- compile_mcnSSD.m
+++ compile_mcnSSD.m
@@ -1,18 +1,63 @@
 function compile_mcnSSD(varargin)
-% compile the C++/CUDA components of the SSD Detector 
+% COMPILE_MCNSSD compile the C++/CUDA components of the SSD Detector 
+%
+% Copyright (C) 2017 Samuel Albanie
+% Licensed under The MIT License [see LICENSE.md for details]
 
-last_args_path = fullfile(vl_rootnn, 'matlab/mex/.build', ...
-  'last_compile_opts.mat') ;
-opts = {} ;
+tokens = {vl_rootnn, 'matlab', 'mex', '.build', 'last_compile_opts.mat'} ;
+last_args_path = fullfile(tokens{:}) ; opts = {} ;
+if exist(last_args_path, 'file'), opts = {load(last_args_path)} ; end
+opts = selectCompileOpts(opts) ;
+vl_compilenn(opts{:}, varargin{:}, 'preCompileFn', @preCompileFn) ;
 
-if exist(last_args_path, 'file')
-  opts = {load(last_args_path)} ;
+% ------------------------------------------------------------------------
+function [opts, mex_src, lib_src, flags] = preCompileFn(opts, mex_src, ...
+                                                        lib_src, flags)
+% ------------------------------------------------------------------------
+mcn_root = vl_rootnn() ;
+root = fullfile(fileparts(mfilename('fullpath')), 'matlab') ;
+
+% Build inside the module path
+flags.src_dir = fullfile(root, 'src') ;
+flags.mex_dir = fullfile(root, 'mex') ;
+flags.bld_dir = fullfile(flags.mex_dir, '.build') ;
+implDir = fullfile(flags.bld_dir,'bits/impl') ;
+if ~exist(implDir, 'dir'), mkdir(implDir) ; end
+mex_src = {} ; lib_src = {} ; 
+if opts.enableGpu, ext = 'cu' ;  else, ext = 'cpp' ; end
+
+% Add mcn dependencies
+lib_src{end+1} = fullfile(mcn_root, 'matlab/src/bits', ['data.' ext]) ;
+lib_src{end+1} = fullfile(mcn_root, 'matlab/src/bits', ['datamex.' ext]) ;
+lib_src{end+1} = fullfile(mcn_root,'matlab/src/bits/impl/copy_cpu.cpp') ;
+
+if opts.enableGpu
+  lib_src{end+1} = fullfile(mcn_root,'matlab/src/bits/datacu.cu') ;
+  lib_src{end+1} = fullfile(mcn_root,'matlab/src/bits/impl/copy_gpu.cu') ;
 end
 
-% select relevant options from original compilation
-opts = selectCompileOpts(opts) ;
+% ----------------------
+% include required files
+% ----------------------
+% old style include - leave as comment in case users have different setup
+%if ~isfield(flags, 'cc'), flags.cc = {inc} ; else, flags.cc{end+1} = inc ; end 
 
-vl_compilenn(opts{:}, varargin{:}, 'preCompileFn', @preCompileFn) ;
+% new style include
+inc = sprintf('-I"%s"', fullfile(mcn_root,'matlab','src')) ;
+inc_local = sprintf('-I"%s"', fullfile(root, 'src')) ;
+flags.base{end+1} = inc ; flags.base{end+1} = inc_local ;
+
+% Add module files
+lib_src{end+1} = fullfile(root,'src/bits',['nnmultiboxdetector.' ext]) ;
+mex_src{end+1} = fullfile(root,'src',['vl_nnmultiboxdetector.' ext]) ;
+
+% CPU-specific files
+lib_src{end+1} = fullfile(root,'src/bits/impl/multiboxdetector_cpu.cpp') ;
+
+% GPU-specific files
+if opts.enableGpu
+  lib_src{end+1} = fullfile(root,'src/bits/impl/multiboxdetector_gpu.cu') ;
+end
 
 % -------------------------------------
 function opts = selectCompileOpts(opts) 
@@ -30,52 +75,3 @@ for i = 1:numel(f)
 end
 opts = {s} ;
 
-% ------------------------------------------------------------------------
-function [opts, mex_src, lib_src, flags] = preCompileFn(opts, mex_src, ...
-                                                        lib_src, flags)
-% ------------------------------------------------------------------------
-
-mcn_root = vl_rootnn() ;
-root = fullfile(fileparts(mfilename('fullpath')), 'matlab') ;
-
-% Build inside the module path
-flags.src_dir = fullfile(root, 'src') ;
-flags.mex_dir = fullfile(root, 'mex') ;
-flags.bld_dir = fullfile(flags.mex_dir, '.build') ;
-
-if ~exist(fullfile(flags.bld_dir,'bits/impl'), 'dir')
-  mkdir(fullfile(flags.bld_dir,'bits/impl')) ;
-end
-
-mex_src = {} ;
-lib_src = {} ; 
-
-if opts.enableGpu
-  ext = 'cu' ; 
-else 
-  ext = 'cpp' ; 
-end
-
-% Add mcn dependencies
-lib_src{end+1} = fullfile(mcn_root, 'matlab/src/bits', ['data.' ext]) ;
-lib_src{end+1} = fullfile(mcn_root, 'matlab/src/bits', ['datamex.' ext]) ;
-lib_src{end+1} = fullfile(mcn_root,'matlab/src/bits/impl/copy_cpu.cpp') ;
-
-if opts.enableGpu
-  lib_src{end+1} = fullfile(mcn_root,'matlab/src/bits/datacu.cu') ;
-  lib_src{end+1} = fullfile(mcn_root,'matlab/src/bits/impl/copy_gpu.cu') ;
-end
-
-flags.cc{end+1} = sprintf('-I%s', fullfile(mcn_root,'matlab/src')) ;
-
-% Add module files
-lib_src{end+1} = fullfile(root,'src/bits',['nnmultiboxdetector.' ext]) ;
-mex_src{end+1} = fullfile(root,'src',['vl_nnmultiboxdetector.' ext]) ;
-
-% CPU-specific files
-lib_src{end+1} = fullfile(root,'src/bits/impl/multiboxdetector_cpu.cpp') ;
-
-% GPU-specific files
-if opts.enableGpu
-  lib_src{end+1} = fullfile(root,'src/bits/impl/multiboxdetector_gpu.cu') ;
-end
