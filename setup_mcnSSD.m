c7b719fc761876136e01172902ae31ec7400892b
albanie <albanie@robots.ox.ac.uk>
2017-09-21 08:47:03 +0100

    fix paths

--- setup_mcnSSD.m
+++ setup_mcnSSD.m
@@ -2,6 +2,6 @@ function setup_mcnSSD
 %SETUP_MCNSSD Sets up mcnSSD by adding its folders to the MATLAB path
 
   root = fileparts(mfilename('fullpath')) ;
-  addpath(root, [root '/misc']) ;
-  addpath([root '/core'], [root '/pascal'], [root '/matlab']) ;
+  addpath(root, [root '/misc'], [root '/matlab']) ;
+  addpath([root '/core'], [root '/pascal'], [root '/pascal/helpers']) ;
   addpath([root '/matlab/utils'], [root '/matlab/mex']) ;
