0dc7fd5c56cc1819548cb741524902d2b2d67968
albanie <albanie@robots.ox.ac.uk>
2017-10-16 17:14:38 +0100

    updated tests

--- matlab/xtest/suite/nnmultiboxloss.m
+++ matlab/xtest/suite/nnmultiboxloss.m
@@ -4,17 +4,13 @@ classdef nnmultiboxloss < nntest
     function basic(test)
       x = test.randn(1) ;
       l = test.randn(1) ;
-      y = vl_nnmultiboxloss(x, l, []) ;
+      y = vl_nnmultiboxloss(x, l) ;
 
       % check derivatives with numerical approximation
       dzdy = test.randn(size(y)) ;
-
-      ders = vl_nnmultiboxloss(x, l, dzdy) ;
-      dzdx = ders{1} ;
-      dzdl = ders{2} ;
-
-      test.der(@(x) vl_nnmultiboxloss(x, l, []), x, dzdy, dzdx, 1e-3*test.range) ;
-      test.der(@(l) vl_nnmultiboxloss(x, l, []), l, dzdy, dzdl, 1e-3*test.range) ;
+      [dzdx, dzdl] = vl_nnmultiboxloss(x, l, dzdy) ;
+      test.der(@(x) vl_nnmultiboxloss(x, l), x, dzdy, dzdx, 1e-3*test.range) ;
+      test.der(@(l) vl_nnmultiboxloss(x, l), l, dzdy, dzdl, 1e-3*test.range) ;
     end
   end
 end
